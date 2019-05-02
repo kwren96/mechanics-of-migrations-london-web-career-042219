8b575fefe98a303f733aa23554242337570b47c1
Katherine Wren <kwren@outlook.com>
2019-05-02 13:44:14 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[05]
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.0]
   def change
     add_column :artists, :favorite_food, :string
   end
